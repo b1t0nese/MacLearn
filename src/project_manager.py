27427a973f661c5d43eddf9fe03ba6452ae98ff5
B1t0ne <111574736+ArtemChikc@users.noreply.github.com>
2025-10-31 22:32:21 +0500

    first tab ready
    - Сохранение проекта как и существующего так и в новый путь
    - Открытие другого проекта в той же сессии
    - Полное изменение настроек датасета в первой вкладке от классов до кол-ва изображений на класс
    - Сохранение проекта
    - Показ по какому пути сейчас запущен проект
    - Диалоговые окна для предотвращения ненужных действий
    - Начата разработка модуля для работы с проектом (на данный момент только с настройкой)

---
+++ src/project_manager.py
@@ -0,0 +1,182 @@
+from pathlib import Path
+import shutil
+import json
+import os
+import re
+
+
+
+def to_snake_case(text: str) -> str:
+    translit_dict = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+        'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+        'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
+        'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+        ' ': '_'
+    }
+    text = text
+    result = []
+    for char in text.lower():
+        if char in translit_dict:
+            result.append(translit_dict[char])
+        elif char.isalnum():
+            result.append(char)
+        else:
+            result.append('_')
+    snake_text = ''.join(result)
+    snake_text = re.sub(r'_+', '_', snake_text)
+    return snake_text.strip('_')
+
+
+
+class Project:
+    def __init__(self, path: str):
+        self.project_path = path
+        self.project_paths = {
+            "dataset": {
+                "train": {},
+                "val": {}
+            },
+            "labels": {
+                "train": {},
+                "val": {}
+            },
+            "xexample_images": {},
+            "dataset.yaml": None,
+            "project.conf": None
+        }
+        self.makedirs_ifnotexc()
+        self.add_skipped_paths()
+        self.load()
+
+
+    def makedirs_ifnotexc(self):
+        base = Path(self.project_path)
+        def process_structure(current_path, structure):
+            for key, value in structure.items():
+                item_path = current_path / key
+                if isinstance(value, dict):
+                    item_path.mkdir(parents=True, exist_ok=True)
+                    process_structure(item_path, value)
+                elif value is None:
+                    item_path.touch(exist_ok=True)
+        process_structure(base, self.project_paths)
+        return True
+
+    def add_skipped_paths(self):
+        base = Path(self.project_path)
+        def scan_and_add_paths(current_path, structure_node):
+            if current_path.exists() and current_path.is_dir():
+                for item in current_path.iterdir():
+                    if item.name not in structure_node:
+                        if item.is_dir():
+                            structure_node[item.name] = {}
+                            scan_and_add_paths(item, structure_node[item.name])
+                        elif item.is_file():
+                            structure_node[item.name] = None
+        for key, value in self.project_paths.items():
+            if isinstance(value, dict):
+                folder_path = base / key
+                scan_and_add_paths(folder_path, self.project_paths[key])
+            elif value is None:
+                file_path = base / key
+                if file_path.exists() and file_path.name not in self.project_paths:
+                    self.project_paths[file_path.name] = None
+
+    def get_full_path(self, *path_parts: str) -> str:
+        current_level = self.project_paths
+        full_path = Path(self.project_path)
+        for part in path_parts:
+            if part in current_level:
+                full_path = full_path / part
+                if isinstance(current_level[part], dict):
+                    current_level = current_level[part]
+                else: break
+            else: return None
+        return str(full_path).replace('\\', '/')
+
+
+    def path_is_project(project_path: str) -> bool:
+        return os.path.exists(os.path.join(project_path, "project.conf"))
+
+    def load(self) -> bool:
+        try:
+            with open(self.get_full_path("project.conf"), "r", encoding="utf-8") as f:
+                self.project = json.load(f)
+        except:
+            self.project = {
+                "classes": [],
+                "dataset_settings": {
+                    "validation_data": True,
+                    "augmented_images": False,
+                    "annotation": True,
+                    "annotation_format": "YOLO",
+                    "images_per_class": 100
+                }
+            }
+            self.save(self.project)
+
+    def save(self, data: dict) -> bool:
+        for i, class_data in enumerate(data["classes"]):
+            for j, subclass_data in enumerate(class_data["subclasses"]):
+                if "/" in subclass_data["example_image"]:
+                    data["classes"][i]["subclasses"][j]["example_image"] = self.add_subclass_example_image(subclass_data)
+        self.add_skipped_paths()
+        for file in self.project_paths["xexample_images"].keys():
+            if file not in self.get_all_example_images(data):
+                os.remove(self.get_full_path("xexample_images", file))
+        self.add_skipped_paths()
+        self.project = data
+        with open(self.get_full_path("project.conf"), "w", encoding="utf-8") as f:
+            f.write(json.dumps(data, indent=2, ensure_ascii=False))
+
+    def save_as(self, to_path: str) -> tuple:
+        try:
+            os.makedirs(to_path, exist_ok=True)
+            for item in os.listdir(self.project_path):
+                source_item = os.path.join(self.project_path, item)
+                dest_item = os.path.join(to_path, item)
+                if os.path.isdir(source_item):
+                    shutil.copytree(source_item, dest_item, dirs_exist_ok=True)
+                else:
+                    shutil.copy2(source_item, dest_item)
+            return True, f"Датасет успешно сохранён по пути: {to_path}"
+        except Exception as e:
+            return False, f"Проект не был сохранён по новому пути. Ошибка: {e}"
+
+
+    def add_subclass_example_image(self, subclass_data: dict) -> str:
+        try:
+            img_path = subclass_data["example_image"]
+            new_path = self.get_full_path("xexample_images")
+            new_filename = to_snake_case(subclass_data["search_query"])+\
+                "."+os.path.basename(img_path).split(".")[-1]
+            new_path = os.path.join(new_path, new_filename)
+            shutil.copy(img_path, new_path)
+            return new_filename
+        except shutil.SameFileError:
+            return os.path.basename(subclass_data["example_image"])
+
+    def get_all_example_images(self, data) -> set:
+        example_images = set()
+        def find_example_images(obj):
+            if isinstance(obj, dict):
+                if "example_image" in obj and isinstance(obj["example_image"], str):
+                    example_images.add(obj["example_image"])
+                for value in obj.values():
+                    find_example_images(value)
+            elif isinstance(obj, list):
+                for item in obj:
+                    find_example_images(item)
+        find_example_images(data)
+        return example_images
+
+
+    def generate_yaml_config(self) -> bool:
+        """Генерирует dataset.yaml для YOLO"""
+        pass
+
+    def export_dataset(self, export_format: str = "YOLO") -> bool:
+        """Экспортирует датасет в указанном формате"""
+        pass
--- src/window.py
+++ src/window.py
@@ -1,6 +1,7 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QMainWindow, QWidget, QLabel, QFileDialog
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtCore import QTimer, Qt
 import os
 
 uis_path = os.path.join(os.path.dirname(__file__), "uis")
@@ -10,26 +11,38 @@ uis_path = os.path.join(os.path.dirname(__file__), "uis")
 class MainWindowUI(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
 
     def initUI(self):
         uic.loadUi(os.path.join(uis_path, "mainwindow.ui"), self)
-
         self.project_tab = uic.loadUi(os.path.join(uis_path, "tabs", "project_tab.ui"))
         self.tabWidget.addTab(self.project_tab, "Проект")
-
         self.project_tab.btn_add_class.clicked.connect(self.add_class)
-
+        self.project_tab.class_objects = []
         self.resize_timer = QTimer()
         self.resize_timer.setSingleShot(True)
         self.resize_timer.timeout.connect(self.update_all_class_layouts)
 
+    def set_image(self, image_label: QLabel, image_path: str) -> bool:
+        try:
+            pixmap = QPixmap.fromImage(QImage(image_path))
+            image_label.setPixmap(
+                pixmap.scaled(
+                    image_label.parent().width(), image_label.parent().height(),
+                    Qt.AspectRatioMode.KeepAspectRatio,
+                    Qt.TransformationMode.SmoothTransformation,
+                ))
+            image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            image_label.my_image_path = image_path
+            return True
+        except Exception as e:
+            image_label.setText(f"Ошибка: {e}")
+            return False
 
-    # ==== РАБОТА С КЛАССАМИ В ТАБЕ "Проект" ====
 
-    def add_class(self):
+    def add_class(self, class_name: str = None, enabled: bool = True) -> QWidget:
         class_widget = uic.loadUi(os.path.join(uis_path, "widgets", "class_field.ui"))
         self.project_tab.classes_vertical_layout.addWidget(class_widget)
+        self.project_tab.class_objects.append(class_widget)
         class_widget.subclass_widgets = []
         class_widget.grid_layout = class_widget.class_grid_layout
         class_widget.class_checkbox.stateChanged.connect(self.show_class)
@@ -37,7 +50,11 @@ class MainWindowUI(QMainWindow):
             lambda: self.delete_class(class_widget))
         class_widget.class_add_subclass.clicked.connect(
             lambda: self.add_subclass(class_widget))
+        if class_name:
+            class_widget.class_name.setText(class_name)
+        class_widget.class_checkbox.setChecked(enabled)
         self.resize_timer.start(100)
+        return class_widget
 
     def show_class(self):
         checkbox = self.sender()
@@ -53,13 +70,31 @@ class MainWindowUI(QMainWindow):
         class_widget.subclass_widgets.clear()
         class_widget.setParent(None)
         class_widget.deleteLater()
+        self.project_tab.class_objects.remove(class_widget)
 
-    def add_subclass(self, parent_class):
+    def add_subclass(self, parent_class, search_text: str = None,
+                     subclass_image: str = None) -> QWidget:
         object_card = uic.loadUi(os.path.join(uis_path, "widgets", "object_card.ui"))
         object_card.object_delete.clicked.connect(
             lambda: self.delete_subclass(object_card, parent_class))
         parent_class.subclass_widgets.append(object_card)
+        object_card.object_image.my_image_path = ""
+        if subclass_image:
+            self.set_image(object_card.object_image, subclass_image)
+        object_card.object_image.mousePressEvent = lambda event:\
+            self.subclass_image_click(object_card.object_image, event)
+        if search_text:
+            object_card.object_name.setText(search_text)
         self.update_class_layout(parent_class)
+        return object_card
+
+    def subclass_image_click(self, label, event):
+        file_dialog = QFileDialog(self)
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setNameFilter("Images (*.png *.jpg)")
+        path, ok = file_dialog.getOpenFileName()
+        if ok:
+            self.set_image(label, path)
 
     def delete_subclass(self, subclass_widget, parent_class=None):
         if parent_class is None and hasattr(subclass_widget, 'parent_class'):
