dec2cd8bd6935e833a02328b9e84d96634acfcb3
B1t0ne <111574736+ArtemChikc@users.noreply.github.com>
2025-11-18 20:39:06 +0500

    PEP8 code update

--- src/main.pyw
+++ src/main.pyw
@@ -7,7 +7,6 @@ from project_manager import Project
 from autodataset import AutoDataset
 
 
-
 class App:
     def __init__(self):
         self.autodataset_worker = None
@@ -19,17 +18,17 @@ class App:
         sys.exit(self.appApplication.exec())
 
     def close_application(self, event=None):
-        if hasattr(self, "appApplication") and hasattr(self, "windowUI") and self.appApplication and self.windowUI:
+        if (hasattr(self, "appApplication") and hasattr(self, "windowUI")
+            and self.appApplication and self.windowUI):
             if self.autodataset_worker:
                 self.autodataset_worker.driver.quit()
             self.windowUI.close()
             self.appApplication.quit()
         if event:
-            if hasattr(self.windowUI, 'original_close_event'):
+            if hasattr(self.windowUI, "original_close_event"):
                 self.windowUI.original_close_event(event)
             else:
                 event.accept()
-
 
     def config_window(self):
         self.windowUI.setWindowTitle(f'{self.windowUI.windowTitle()} ("{self.project_data.project_path}")')
@@ -47,10 +46,10 @@ class App:
         for clas in project_conf["classes"]:
             cur_class_widget = self.windowUI.add_class(clas["class_name"], clas["enabled"])
             for subclas in clas["subclasses"]:
-                img_fullpath = self.project_data.get_full_path("example_images", subclas["example_image"])\
-                    if subclas["example_image"] else None
-                self.windowUI.add_subclass(
-                    cur_class_widget, subclas["search_query"], img_fullpath)
+                img_fullpath = (
+                    self.project_data.get_full_path("example_images", subclas["example_image"])
+                    if subclas["example_image"] else None)
+                self.windowUI.add_subclass(cur_class_widget, subclas["search_query"], img_fullpath)
         pr_tab = self.windowUI.project_tab
         pr_tab.cb_validation_data.setChecked(project_conf["configuration"]["validation_data"])
         pr_tab.cb_augmented_images.setChecked(project_conf["configuration"]["augmented_images"])
@@ -59,25 +58,23 @@ class App:
         pr_tab.spin_images_per_class.setValue(project_conf["configuration"]["images_per_class"])
         pr_tab.combo_images_size.setCurrentText(project_conf["configuration"]["images_size"])
 
-
     def open_project(self, project_path: str = None) -> bool:
         if not project_path:
-            project_path = QFileDialog.getExistingDirectory(
-                None, "Выберите папку проекта", "")
+            project_path = QFileDialog.getExistingDirectory(None, "Выберите папку проекта", "")
 
         if project_path:
-            if hasattr(self, 'windowUI') and self.windowUI:
+            if hasattr(self, "windowUI") and self.windowUI:
                 self.windowUI.close()
                 self.windowUI.deleteLater()
-            if hasattr(self, 'autodataset_worker') and self.autodataset_worker:
+            if hasattr(self, "autodataset_worker") and self.autodataset_worker:
                 self.autodataset_worker.driver.quit()
                 self.autodataset_worker = None
             self.windowUI = MainWindowUI()
             if not Project.path_is_project(project_path):
                 reply = QMessageBox.question(
                     self.windowUI, "Проект не найден или повреждён",
-                    f'Проект по заданному пути "{project_path}" не найден, или повреждён. '+\
-                        'Будут созданы недостающие папки и файлы.',
+                    f'Проект по заданному пути "{project_path}" не найден, или повреждён. '
+                    + "Будут созданы недостающие папки и файлы.",
                     QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
                 if reply == QMessageBox.StandardButton.Close:
                     self.close_application()
@@ -105,21 +102,23 @@ class App:
                 "annotation": pr_tab.cb_annotation.isChecked(),
                 "annotation_format": pr_tab.combo_annotation_format.currentText(),
                 "images_per_class": pr_tab.spin_images_per_class.value(),
-                "images_size": pr_tab.combo_images_size.currentText()
+                "images_size": pr_tab.combo_images_size.currentText(),
             },
-            "classes_conf": []
+            "classes_conf": [],
         }
         for i, class_obj in enumerate(self.windowUI.project_tab.class_objects):
             local_class = {
                 "class_name": class_obj.class_name.text(),
                 "enabled": class_obj.class_checkbox.isChecked(),
-                "subclasses": []
+                "subclasses": [],
             }
             for subclass in class_obj.subclass_widgets:
-                local_class["subclasses"].append({
-                    "search_query": subclass.object_name.text(),
-                    "example_image": subclass.object_image.my_image_path
-                })
+                local_class["subclasses"].append(
+                    {
+                        "search_query": subclass.object_name.text(),
+                        "example_image": subclass.object_image.my_image_path,
+                    }
+                )
             local_project_conf["classes_conf"].append(local_class)
         self.project_data.save(**local_project_conf)
         self.windowUI.update_autodataset_statuses()
@@ -138,7 +137,6 @@ class App:
                     self.open_project(new_project_path)
             else:
                 QMessageBox.warning(self.windowUI, "Ошибка", message)
-
 
     def toggle_autodataset_work(self):
         if self.autodataset_worker_thread:
@@ -163,9 +161,9 @@ class App:
         self.windowUI.autodataset_tab.btn_start.setStyleSheet("background-color: #f44336;")
 
     def stop_autodataset(self):
-        if hasattr(self, 'autodataset_worker') and self.autodataset_worker:
+        if hasattr(self, "autodataset_worker") and self.autodataset_worker:
             self.autodataset_worker.stop()
-        if hasattr(self, 'autodataset_thread') and self.autodataset_thread:
+        if hasattr(self, "autodataset_thread") and self.autodataset_thread:
             self.autodataset_worker.finished.connect(self.autodataset_thread.quit)
             self.autodataset_thread.finished.connect(self.autodataset_thread.deleteLater)
             self.autodataset_thread = None
@@ -173,12 +171,10 @@ class App:
         self.windowUI.autodataset_tab.btn_start.setStyleSheet("background-color: #4CAF50;")
 
 
-
 def main():
     application = QApplication(sys.argv)
     app = App()
     app.start(application)
-
 
 
 if __name__ == "__main__":
--- src/window.py
+++ src/window.py
@@ -8,12 +8,11 @@ import sys
 import os
 
 
-if getattr(sys, 'frozen', False):
+if getattr(sys, "frozen", False):
     base_path = sys._MEIPASS
 else:
     base_path = os.path.dirname(__file__)
 uis_path = os.path.join(base_path, "uis")
-
 
 
 class MainWindowUI(QMainWindow):
@@ -38,7 +37,6 @@ class MainWindowUI(QMainWindow):
         self.resize_timer.setSingleShot(True)
         self.resize_timer.timeout.connect(self.update_all_class_layouts)
 
-
     def update_autodataset_statuses(self):
         self.update_autodataset_classes_status()
         self.visualise_autodataset_classes_status()
@@ -56,7 +54,7 @@ class MainWindowUI(QMainWindow):
         header_stages.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
 
     def update_autodataset_classes_status(self):
-        local_classes_status = {}    
+        local_classes_status = {}
         for class_obj in self.project_tab.class_objects:
             if class_obj.class_checkbox.isChecked():
                 for subclass in class_obj.subclass_widgets:
@@ -65,14 +63,15 @@ class MainWindowUI(QMainWindow):
                         "example_image": subclass.object_image.my_image_path,
                         "class": class_obj.class_name.text(),
                         "status": f"{self.autodataset_classes_status[subclass_text]["status"].split("/")[0]\
-                                     if subclass_text in self.autodataset_classes_status else 0}"+\
-                                        f"/{self.project_tab.spin_images_per_class.value()//len(class_obj.subclass_widgets)}"
+                                     if subclass_text in self.autodataset_classes_status else 0}"
+                        + f"/{self.project_tab.spin_images_per_class.value()//len(class_obj.subclass_widgets)}",
                     }
                     local_classes_status[subclass_text] = subclass_data
         self.autodataset_classes_status = local_classes_status.copy()
 
     def update_autodataset_subclass_status(self, subclass_name: str, status: int):
-        self.autodataset_classes_status[subclass_name]["status"] = f"{status}/{self.autodataset_classes_status[subclass_name]["status"].split("/")[1]}"
+        self.autodataset_classes_status[subclass_name]["status"
+        ] = f"{status}/{self.autodataset_classes_status[subclass_name]["status"].split("/")[1]}"
         self.visualise_autodataset_classes_status()
 
     def visualise_autodataset_classes_status(self):
@@ -84,8 +83,9 @@ class MainWindowUI(QMainWindow):
             self.autodataset_tab.table_classes.setItem(r, 1, QTableWidgetItem(subclass_text))
             self.autodataset_tab.table_classes.setItem(r, 2, QTableWidgetItem(subclass["status"]))
 
-    def autodataset_log(self, text: str, otstup: int=0):
-        self.autodataset_tab.text_logs.append('\n'*otstup+f'[{datetime.now().strftime("%H:%M:%S")}] {text}')
+    def autodataset_log(self, text: str, otstup: int = 0):
+        self.autodataset_tab.text_logs.append(
+            "\n" * otstup + f'[{datetime.now().strftime("%H:%M:%S")}] {text}')
 
     def autodataset_set_image(self, image_path: str):
         pixmap = QPixmap.fromImage(QImage(image_path))
@@ -95,22 +95,22 @@ class MainWindowUI(QMainWindow):
                 self.autodataset_tab.label_image.height(),
                 Qt.AspectRatioMode.KeepAspectRatio,
                 Qt.TransformationMode.SmoothTransformation,
-            ))
+        ))
         self.autodataset_tab.label_image.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.autodataset_tab.label_image_path.setText(image_path)
 
-    ... # другие функции специально для "Автодатасет" табы
-
+    ...  # другие функции специально для "Автодатасет" табы
 
     def set_obj_card_image(self, image_label: QLabel, image_path: str) -> bool:
         try:
             pixmap = QPixmap.fromImage(QImage(image_path))
             image_label.setPixmap(
                 pixmap.scaled(
-                    image_label.parent().width(), image_label.parent().height(),
+                    image_label.parent().width(),
+                    image_label.parent().height(),
                     Qt.AspectRatioMode.KeepAspectRatio,
                     Qt.TransformationMode.SmoothTransformation,
-                ))
+            ))
             image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             image_label.my_image_path = image_path
             return True
@@ -125,10 +125,8 @@ class MainWindowUI(QMainWindow):
         class_widget.subclass_widgets = []
         class_widget.grid_layout = class_widget.class_grid_layout
         class_widget.class_checkbox.stateChanged.connect(self.show_class)
-        class_widget.class_delete.clicked.connect(
-            lambda: self.delete_class(class_widget))
-        class_widget.class_add_subclass.clicked.connect(
-            lambda: self.add_subclass(class_widget))
+        class_widget.class_delete.clicked.connect(lambda: self.delete_class(class_widget))
+        class_widget.class_add_subclass.clicked.connect(lambda: self.add_subclass(class_widget))
         if class_name:
             class_widget.class_name.setText(class_name)
         class_widget.class_checkbox.setChecked(enabled)
@@ -155,13 +153,15 @@ class MainWindowUI(QMainWindow):
                      subclass_image: str = None) -> QWidget:
         object_card = uic.loadUi(os.path.join(uis_path, "widgets", "object_card.ui"))
         object_card.object_delete.clicked.connect(
-            lambda: self.delete_subclass(object_card, parent_class))
+            lambda: self.delete_subclass(object_card, parent_class)
+        )
         parent_class.subclass_widgets.append(object_card)
         object_card.object_image.my_image_path = ""
         if subclass_image:
             self.set_obj_card_image(object_card.object_image, subclass_image)
-        object_card.object_image.mousePressEvent = lambda event:\
-            self.subclass_image_click(object_card.object_image, event)
+        object_card.object_image.mousePressEvent = (
+            lambda event: self.subclass_image_click(object_card.object_image, event)
+        )
         if search_text:
             object_card.object_name.setText(search_text)
         self.update_class_layout(parent_class)
@@ -176,7 +176,7 @@ class MainWindowUI(QMainWindow):
             self.set_obj_card_image(label, path)
 
     def delete_subclass(self, subclass_widget, parent_class=None):
-        if parent_class is None and hasattr(subclass_widget, 'parent_class'):
+        if parent_class is None and hasattr(subclass_widget, "parent_class"):
             parent_class = subclass_widget.parent_class
         if parent_class and subclass_widget in parent_class.subclass_widgets:
             parent_class.subclass_widgets.remove(subclass_widget)
@@ -194,9 +194,9 @@ class MainWindowUI(QMainWindow):
         container_width = class_widget.width()
         subclass_width = 270
         spacing = 10
-        if container_width<=0:
+        if container_width <= 0:
             return
-        max_subclasses_per_row = max(1, (container_width - spacing)//(subclass_width+spacing))
+        max_subclasses_per_row = max(1, (container_width - spacing) // (subclass_width + spacing))
 
         for i in reversed(range(class_widget.grid_layout.count())):
             item = class_widget.grid_layout.itemAt(i)
@@ -204,21 +204,21 @@ class MainWindowUI(QMainWindow):
                 item.widget().setParent(None)
 
         add_button = class_widget.class_add_subclass
-        total_widgets = len(class_widget.subclass_widgets)+1
+        total_widgets = len(class_widget.subclass_widgets) + 1
 
         for i, widget in enumerate(class_widget.subclass_widgets):
             row = i // max_subclasses_per_row
             col = i % max_subclasses_per_row
             class_widget.grid_layout.addWidget(widget, row, col)
 
-        last_row = (total_widgets-1)//max_subclasses_per_row
-        last_col = (total_widgets-1)%max_subclasses_per_row
+        last_row = (total_widgets - 1) // max_subclasses_per_row
+        last_col = (total_widgets - 1) % max_subclasses_per_row
         class_widget.grid_layout.addWidget(add_button, last_row, last_col)
 
     def update_all_class_layouts(self):
         for i in range(self.project_tab.classes_vertical_layout.count()):
             item = self.project_tab.classes_vertical_layout.itemAt(i)
-            if item and hasattr(item.widget(), 'subclass_widgets'):
+            if item and hasattr(item.widget(), "subclass_widgets"):
                 self.update_class_layout(item.widget())
 
     def resizeEvent(self, event):
