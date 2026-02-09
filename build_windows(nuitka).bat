c2e606e49419816376f1b7abebfece9020bfc377
b1t0ne <hornid2010@mail.ru>
2026-02-09 18:45:00 +0500

    Fix build script

--- build_windows(nuitka).bat
+++ build_windows(nuitka).bat
@@ -1,23 +1,5 @@
 python -m nuitka --standalone --windows-console-mode=disable ^
-    --noinclude-numba-mode=allow ^
-    --output-dir=build ^
-    --jobs=11 ^
-    --enable-plugin=pyqt6 ^
+    --noinclude-numba-mode=allow --enable-plugin=pyqt6 --follow-imports ^
     --include-data-dir=src/interface_module/uis=interface_module/uis ^
-    --remove-output ^
-    --output-filename=MacLearn.exe ^
-
-    --follow-imports ^
-    --follow-stdlib ^
-    --recurse-none ^
-    --assume-yes-for-downloads ^
-    --user-plugin=remove_unused_imports.py ^
-
-    --lto=yes ^
-    --experimental=use_pefile ^
-    --experimental=use_pefile_recurse ^
-
-    --python-flag=-O ^
-    --python-flag=-B ^
-
-    src/main.pyw
+    --jobs=11 --output-dir=build --remove-output ^
+    --output-filename=MacLearn.exe src/main.pyw
